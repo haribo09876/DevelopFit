533e32a0128ea50300fb94351defb03ecf066454
haribo09876 <haribo09876@gmail.com>
2023-09-08 10:54:27 +0900

    팀 프로젝트 해야 할 일
    팀 프로젝트 적용한 일
    
    내가 맡은 일
    	- 영화 정보 및 DB CRUD 기능
    		영화정보 조회
    		영화정보 입력
    		영화정보 수정
    		영화정보 삭제
    
    내가 적용한 일
    		MovieDto 변경(○, DB변경으로 인한 변화)
    
    작성자 KYW

--- DevelopFit/src/main/java/com/dev/movie/dto/MovieDto.java
+++ DevelopFit/src/main/java/com/dev/movie/dto/MovieDto.java
@@ -3,7 +3,26 @@ package com.dev.movie.dto;
 import java.util.Date;
 
 public class MovieDto {
+	
+	private int actor_number;
+	private String actor_name;
+	
+	private int basket_number;
+	
+	private int cast_member_number;
 
+	private int director_number;
+	private String director_name;
+	
+	private int genre_number;
+	private String genre_name;
+	
+	private int heart_number;
+	private String heart_check;
+	
+	private int line_review_number;
+	private String line_review_context;
+		
 	private int movie_number;
 	private String movie_title;
 	private String movie_summary;
@@ -13,35 +32,47 @@ public class MovieDto {
 	private String movie_poster;
 	private String movie_preview;
 	
-	private int director_number;
-	private String director_name;
+	private int order_history_number;
+	private Date order_history_time;
 	
-	private int actor_number;
-	private String actor_name;
+	private int prefer_genre_number;
+	private String prefer_genre_first;
+	private String prefer_genre_second;
+	private String prefer_genre_third;
 	
 	private int rating_number;
 	private int rating_grade;
 	
-	private int line_review_number;
-	private String line_review_context;
-	private String user_id;
+	private int watch_number;
+	private String watch_check;
 	
-	private int order_history_number;
-	private Date order_history_time;
-	private int user_number;
+	private int member_number;
+	private String member_id;
 	
-	private int basket_number;
-
 	public MovieDto() {
 		super();
 	}
 
-	public MovieDto(int movie_number, String movie_title, String movie_summary, Date movie_release_date,
-			String movie_runtime, int movie_price, String movie_poster, String movie_preview, int director_number,
-			String director_name, int actor_number, String actor_name, int rating_number, int rating_grade,
-			int line_review_number, String line_review_context, String user_id, int order_history_number,
-			Date order_history_time, int user_number, int basket_number) {
+	public MovieDto(int actor_number, String actor_name, int basket_number, int cast_member_number, int director_number,
+			String director_name, int genre_number, String genre_name, int heart_number, String heart_check,
+			int line_review_number, String line_review_context, int movie_number, String movie_title,
+			String movie_summary, Date movie_release_date, String movie_runtime, int movie_price, String movie_poster,
+			String movie_preview, int order_history_number, Date order_history_time, int prefer_genre_number,
+			String prefer_genre_first, String prefer_genre_second, String prefer_genre_third, int rating_number,
+			int rating_grade, int watch_number, String watch_check, int member_number, String member_id) {
 		super();
+		this.actor_number = actor_number;
+		this.actor_name = actor_name;
+		this.basket_number = basket_number;
+		this.cast_member_number = cast_member_number;
+		this.director_number = director_number;
+		this.director_name = director_name;
+		this.genre_number = genre_number;
+		this.genre_name = genre_name;
+		this.heart_number = heart_number;
+		this.heart_check = heart_check;
+		this.line_review_number = line_review_number;
+		this.line_review_context = line_review_context;
 		this.movie_number = movie_number;
 		this.movie_title = movie_title;
 		this.movie_summary = movie_summary;
@@ -50,19 +81,114 @@ public class MovieDto {
 		this.movie_price = movie_price;
 		this.movie_poster = movie_poster;
 		this.movie_preview = movie_preview;
-		this.director_number = director_number;
-		this.director_name = director_name;
-		this.actor_number = actor_number;
-		this.actor_name = actor_name;
-		this.rating_number = rating_number;
-		this.rating_grade = rating_grade;
-		this.line_review_number = line_review_number;
-		this.line_review_context = line_review_context;
-		this.user_id = user_id;
 		this.order_history_number = order_history_number;
 		this.order_history_time = order_history_time;
-		this.user_number = user_number;
+		this.prefer_genre_number = prefer_genre_number;
+		this.prefer_genre_first = prefer_genre_first;
+		this.prefer_genre_second = prefer_genre_second;
+		this.prefer_genre_third = prefer_genre_third;
+		this.rating_number = rating_number;
+		this.rating_grade = rating_grade;
+		this.watch_number = watch_number;
+		this.watch_check = watch_check;
+		this.member_number = member_number;
+		this.member_id = member_id;
+	}
+
+	public int getActor_number() {
+		return actor_number;
+	}
+
+	public void setActor_number(int actor_number) {
+		this.actor_number = actor_number;
+	}
+
+	public String getActor_name() {
+		return actor_name;
+	}
+
+	public void setActor_name(String actor_name) {
+		this.actor_name = actor_name;
+	}
+
+	public int getBasket_number() {
+		return basket_number;
+	}
+
+	public void setBasket_number(int basket_number) {
 		this.basket_number = basket_number;
+	}
+
+	public int getCast_member_number() {
+		return cast_member_number;
+	}
+
+	public void setCast_member_number(int cast_member_number) {
+		this.cast_member_number = cast_member_number;
+	}
+
+	public int getDirector_number() {
+		return director_number;
+	}
+
+	public void setDirector_number(int director_number) {
+		this.director_number = director_number;
+	}
+
+	public String getDirector_name() {
+		return director_name;
+	}
+
+	public void setDirector_name(String director_name) {
+		this.director_name = director_name;
+	}
+
+	public int getGenre_number() {
+		return genre_number;
+	}
+
+	public void setGenre_number(int genre_number) {
+		this.genre_number = genre_number;
+	}
+
+	public String getGenre_name() {
+		return genre_name;
+	}
+
+	public void setGenre_name(String genre_name) {
+		this.genre_name = genre_name;
+	}
+
+	public int getHeart_number() {
+		return heart_number;
+	}
+
+	public void setHeart_number(int heart_number) {
+		this.heart_number = heart_number;
+	}
+
+	public String getHeart_check() {
+		return heart_check;
+	}
+
+	public void setHeart_check(String heart_check) {
+		this.heart_check = heart_check;
+	}
+
+	public int getLine_review_number() {
+		return line_review_number;
+	}
+
+	public void setLine_review_number(int line_review_number) {
+		this.line_review_number = line_review_number;
+	}
+
+	public String getLine_review_context() {
+		return line_review_context;
+	}
+
+	public void setLine_review_context(String line_review_context) {
+		this.line_review_context = line_review_context;
 	}
 
 	public int getMovie_number() {
@@ -129,36 +255,52 @@ public class MovieDto {
 		this.movie_preview = movie_preview;
 	}
 
-	public int getDirector_number() {
-		return director_number;
+	public int getOrder_history_number() {
+		return order_history_number;
 	}
 
-	public void setDirector_number(int director_number) {
-		this.director_number = director_number;
+	public void setOrder_history_number(int order_history_number) {
+		this.order_history_number = order_history_number;
 	}
 
-	public String getDirector_name() {
-		return director_name;
+	public Date getOrder_history_time() {
+		return order_history_time;
 	}
 
-	public void setDirector_name(String director_name) {
-		this.director_name = director_name;
+	public void setOrder_history_time(Date order_history_time) {
+		this.order_history_time = order_history_time;
 	}
 
-	public int getActor_number() {
-		return actor_number;
+	public int getPrefer_genre_number() {
+		return prefer_genre_number;
 	}
 
-	public void setActor_number(int actor_number) {
-		this.actor_number = actor_number;
+	public void setPrefer_genre_number(int prefer_genre_number) {
+		this.prefer_genre_number = prefer_genre_number;
 	}
 
-	public String getActor_name() {
-		return actor_name;
+	public String getPrefer_genre_first() {
+		return prefer_genre_first;
 	}
 
-	public void setActor_name(String actor_name) {
-		this.actor_name = actor_name;
+	public void setPrefer_genre_first(String prefer_genre_first) {
+		this.prefer_genre_first = prefer_genre_first;
+	}
+
+	public String getPrefer_genre_second() {
+		return prefer_genre_second;
+	}
+
+	public void setPrefer_genre_second(String prefer_genre_second) {
+		this.prefer_genre_second = prefer_genre_second;
+	}
+
+	public String getPrefer_genre_third() {
+		return prefer_genre_third;
+	}
+
+	public void setPrefer_genre_third(String prefer_genre_third) {
+		this.prefer_genre_third = prefer_genre_third;
 	}
 
 	public int getRating_number() {
@@ -177,73 +319,53 @@ public class MovieDto {
 		this.rating_grade = rating_grade;
 	}
 
-	public int getLine_review_number() {
-		return line_review_number;
+	public int getWatch_number() {
+		return watch_number;
 	}
 
-	public void setLine_review_number(int line_review_number) {
-		this.line_review_number = line_review_number;
+	public void setWatch_number(int watch_number) {
+		this.watch_number = watch_number;
 	}
 
-	public String getLine_review_context() {
-		return line_review_context;
+	public String getWatch_check() {
+		return watch_check;
 	}
 
-	public void setLine_review_context(String line_review_context) {
-		this.line_review_context = line_review_context;
+	public void setWatch_check(String watch_check) {
+		this.watch_check = watch_check;
 	}
 
-	public String getUser_id() {
-		return user_id;
+	public int getMember_number() {
+		return member_number;
 	}
 
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
+	public void setMember_number(int member_number) {
+		this.member_number = member_number;
 	}
 
-	public int getOrder_history_number() {
-		return order_history_number;
+	public String getMember_id() {
+		return member_id;
 	}
 
-	public void setOrder_history_number(int order_history_number) {
-		this.order_history_number = order_history_number;
-	}
-
-	public Date getOrder_history_time() {
-		return order_history_time;
-	}
-
-	public void setOrder_history_time(Date order_history_time) {
-		this.order_history_time = order_history_time;
-	}
-
-	public int getUser_number() {
-		return user_number;
-	}
-
-	public void setUser_number(int user_number) {
-		this.user_number = user_number;
-	}
-
-	public int getBasket_number() {
-		return basket_number;
-	}
-
-	public void setBasket_number(int basket_number) {
-		this.basket_number = basket_number;
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
 	}
 
 	@Override
 	public String toString() {
-		return "MovieDto [movie_number=" + movie_number + ", movie_title=" + movie_title + ", movie_summary="
-				+ movie_summary + ", movie_release_date=" + movie_release_date + ", movie_runtime=" + movie_runtime
-				+ ", movie_price=" + movie_price + ", movie_poster=" + movie_poster + ", movie_preview=" + movie_preview
-				+ ", director_number=" + director_number + ", director_name=" + director_name + ", actor_number="
-				+ actor_number + ", actor_name=" + actor_name + ", rating_number=" + rating_number + ", rating_grade="
-				+ rating_grade + ", line_review_number=" + line_review_number + ", line_review_context="
-				+ line_review_context + ", user_id=" + user_id + ", order_history_number=" + order_history_number
-				+ ", order_history_time=" + order_history_time + ", user_number=" + user_number + ", basket_number="
-				+ basket_number + "]";
+		return "MovieDto [actor_number=" + actor_number + ", actor_name=" + actor_name + ", basket_number="
+				+ basket_number + ", cast_member_number=" + cast_member_number + ", director_number=" + director_number
+				+ ", director_name=" + director_name + ", genre_number=" + genre_number + ", genre_name=" + genre_name
+				+ ", heart_number=" + heart_number + ", heart_check=" + heart_check + ", line_review_number="
+				+ line_review_number + ", line_review_context=" + line_review_context + ", movie_number=" + movie_number
+				+ ", movie_title=" + movie_title + ", movie_summary=" + movie_summary + ", movie_release_date="
+				+ movie_release_date + ", movie_runtime=" + movie_runtime + ", movie_price=" + movie_price
+				+ ", movie_poster=" + movie_poster + ", movie_preview=" + movie_preview + ", order_history_number="
+				+ order_history_number + ", order_history_time=" + order_history_time + ", prefer_genre_number="
+				+ prefer_genre_number + ", prefer_genre_first=" + prefer_genre_first + ", prefer_genre_second="
+				+ prefer_genre_second + ", prefer_genre_third=" + prefer_genre_third + ", rating_number="
+				+ rating_number + ", rating_grade=" + rating_grade + ", watch_number=" + watch_number + ", watch_check="
+				+ watch_check + ", member_number=" + member_number + ", member_id=" + member_id + "]";
 	}
 	
 }
