02061e55a8026566bdcf4bf10b9fbd244527a90d
haribo09876 <haribo09876@gmail.com>
2023-09-12 15:51:42 +0900

    by KYW

--- DevelopFit/src/main/webapp/WEB-INF/doc/DF_TABLE.sql
+++ DevelopFit/src/main/webapp/WEB-INF/doc/DF_TABLE.sql
@@ -1,9 +1,9 @@
 --사용자 계정 생성 및 계정 비밀번호 변경, 잠금 해제
-CREATE USER DEVELOPFIT IDENTIFIED BY DEVELOPFIT ACCOUNT UNLOCK;
+CREATE USER df IDENTIFIED BY df12 ACCOUNT UNLOCK;
 
 --계정에 접근 가능 권한 부여 (생성한 사용자가 데이터베이스에 접속할 수 있도록 기본적인 시스템 권한 8가지를 부여)
 --계정에 객체 생성 권한 설정 (생성한 사용자에 대하여 객체(테이블, 뷰, 인덱스등)을 생성할 수 있도록 시스템 권한을 부여)
-GRANT CONNECT, RESOURCE, DBA TO DEVELOPFIT;
+GRANT CONNECT, RESOURCE, DBA TO df;
 
 /* Drop Tables */
 
