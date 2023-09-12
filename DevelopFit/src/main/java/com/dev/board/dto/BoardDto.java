f6d04b0c5175fc89d4b11fcd80157498314dbaa1
Dlgustn <skfnxhwpfh@naver.com>
2023-09-12 16:04:44 +0900

    by LHS

--- DevelopFit/src/main/java/com/dev/board/dto/BoardDto.java
+++ DevelopFit/src/main/java/com/dev/board/dto/BoardDto.java
@@ -4,83 +4,84 @@ import java.util.Date;
 
 public class BoardDto {
 	
-	private int board_number;
-	private String board_title;
-	private String board_content;
-	private Date board_create_date;
-	private int board_hit;
-	private int member_number;
+	private int boardNumber;
+	private String boardTitle;
+	private String boardContent;
+	private Date boardCreateDate;
+	private int boardHit;
+	private int memberNumber;
 	
 	public BoardDto() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public BoardDto(int board_number, String board_title, String board_content,
-			Date board_create_date, int board_hit, int member_number) {
+	public BoardDto(int boardNumber, String boardTitle, String boardContent, Date boardCreateDate, int boardHit,
+			int memberNumber) {
 		super();
-		this.board_number = board_number;
-		this.board_title = board_title;
-		this.board_content = board_content;
-		this.board_create_date = board_create_date;
-		this.board_hit = board_hit;
-		this.member_number = member_number;
+		this.boardNumber = boardNumber;
+		this.boardTitle = boardTitle;
+		this.boardContent = boardContent;
+		this.boardCreateDate = boardCreateDate;
+		this.boardHit = boardHit;
+		this.memberNumber = memberNumber;
 	}
 
-	public int getBoard_number() {
-		return board_number;
+	public int getBoardNumber() {
+		return boardNumber;
 	}
 
-	public void setBoard_number(int board_number) {
-		this.board_number = board_number;
+	public void setBoardNumber(int boardNumber) {
+		this.boardNumber = boardNumber;
 	}
 
-	public String getBoard_title() {
-		return board_title;
+	public String getBoardTitle() {
+		return boardTitle;
 	}
 
-	public void setBoard_title(String board_title) {
-		this.board_title = board_title;
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
 	}
 
-	public String getBoard_content() {
-		return board_content;
+	public String getBoardContent() {
+		return boardContent;
 	}
 
-	public void setBoard_content(String board_content) {
-		this.board_content = board_content;
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
 	}
 
-	public Date getBoard_create_date() {
-		return board_create_date;
+	public Date getBoardCreateDate() {
+		return boardCreateDate;
 	}
 
-	public void setBoard_create_date(Date board_create_date) {
-		this.board_create_date = board_create_date;
+	public void setBoardCreateDate(Date boardCreateDate) {
+		this.boardCreateDate = boardCreateDate;
 	}
 
-	public int getBoard_hit() {
-		return board_hit;
+	public int getBoardHit() {
+		return boardHit;
 	}
 
-	public void setBoard_hit(int board_hit) {
-		this.board_hit = board_hit;
+	public void setBoardHit(int boardHit) {
+		this.boardHit = boardHit;
 	}
 
-	public int getMember_number() {
-		return member_number;
+	public int getMemberNumber() {
+		return memberNumber;
 	}
 
-	public void setMember_number(int member_number) {
-		this.member_number = member_number;
+	public void setMemberNumber(int memberNumber) {
+		this.memberNumber = memberNumber;
 	}
 
 	@Override
 	public String toString() {
-		return "BoardDto [board_number=" + board_number + ", member_id=" + ", board_title=" + board_title
-				+ ", board_content=" + board_content + ", board_create_date=" + board_create_date + ", board_hit="
-				+ board_hit + ", member_number=" + member_number + "]";
+		return "BoardDto [boardNumber=" + boardNumber + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
+				+ ", boardCreateDate=" + boardCreateDate + ", boardHit=" + boardHit + ", memberNumber=" + memberNumber
+				+ "]";
 	}
+	
+	
 	
 	
 	
