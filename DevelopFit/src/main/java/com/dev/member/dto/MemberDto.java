a96601513a1af74e0a4c6b2a3e1bf37a1f33f38d
Ktyun <kvb5504@gmail.com>
2023-09-25 18:23:25 +0900

    dev_member, MemberDto 수정

--- DevelopFit/src/main/java/com/dev/member/dto/MemberDto.java
+++ DevelopFit/src/main/java/com/dev/member/dto/MemberDto.java
@@ -14,17 +14,18 @@ public class MemberDto {
 	String memberEmail;
 	Date memberBirthDate;
 	String memberPhoneNumber;
+	int memberMoney;
 	String memberGender;
 	Date memberCreateDate;
 	Date memberModifyDate;
+	
 	
 	public MemberDto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
 	public MemberDto(int memberNumber, String memberId, String memberPassword, String memberName, String memberEmail,
-			Date memberBirthDate, String memberPhoneNumber, String memberGender, Date memberCreateDate,
+			Date memberBirthDate, String memberPhoneNumber, int memberMoney, String memberGender, Date memberCreateDate,
 			Date memberModifyDate) {
 		super();
 		this.memberNumber = memberNumber;
@@ -34,96 +35,86 @@ public class MemberDto {
 		this.memberEmail = memberEmail;
 		this.memberBirthDate = memberBirthDate;
 		this.memberPhoneNumber = memberPhoneNumber;
+		this.memberMoney = memberMoney;
 		this.memberGender = memberGender;
 		this.memberCreateDate = memberCreateDate;
 		this.memberModifyDate = memberModifyDate;
 	}
-
 	public int getMemberNumber() {
 		return memberNumber;
 	}
-
 	public void setMemberNumber(int memberNumber) {
 		this.memberNumber = memberNumber;
 	}
-
 	public String getMemberId() {
 		return memberId;
 	}
-
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
-
 	public String getMemberPassword() {
 		return memberPassword;
 	}
-
 	public void setMemberPassword(String memberPassword) {
 		this.memberPassword = memberPassword;
 	}
-
 	public String getMemberName() {
 		return memberName;
 	}
-
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
-
 	public String getMemberEmail() {
 		return memberEmail;
 	}
-
 	public void setMemberEmail(String memberEmail) {
 		this.memberEmail = memberEmail;
 	}
-
 	public Date getMemberBirthDate() {
 		return memberBirthDate;
 	}
-
 	public void setMemberBirthDate(Date memberBirthDate) {
 		this.memberBirthDate = memberBirthDate;
 	}
-
 	public String getMemberPhoneNumber() {
 		return memberPhoneNumber;
 	}
-
 	public void setMemberPhoneNumber(String memberPhoneNumber) {
 		this.memberPhoneNumber = memberPhoneNumber;
 	}
-
+	public int getMemberMoney() {
+		return memberMoney;
+	}
+	public void setMemberMoney(int memberMoney) {
+		this.memberMoney = memberMoney;
+	}
 	public String getMemberGender() {
 		return memberGender;
 	}
-
 	public void setMemberGender(String memberGender) {
 		this.memberGender = memberGender;
 	}
-
 	public Date getMemberCreateDate() {
 		return memberCreateDate;
 	}
-
 	public void setMemberCreateDate(Date memberCreateDate) {
 		this.memberCreateDate = memberCreateDate;
 	}
-
 	public Date getMemberModifyDate() {
 		return memberModifyDate;
 	}
-
 	public void setMemberModifyDate(Date memberModifyDate) {
 		this.memberModifyDate = memberModifyDate;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "MemberDto [memberNumber=" + memberNumber + ", memberId=" + memberId + ", memberPassword="
 				+ memberPassword + ", memberName=" + memberName + ", memberEmail=" + memberEmail + ", memberBirthDate="
-				+ memberBirthDate + ", memberPhoneNumber=" + memberPhoneNumber + ", memberGender=" + memberGender
-				+ ", memberCreateDate=" + memberCreateDate + ", memberModifyDate=" + memberModifyDate + "]";
-	}	
+				+ memberBirthDate + ", memberPhoneNumber=" + memberPhoneNumber + ", memberMoney=" + memberMoney
+				+ ", memberGender=" + memberGender + ", memberCreateDate=" + memberCreateDate + ", memberModifyDate="
+				+ memberModifyDate + "]";
+	}
+	
+	
 }
